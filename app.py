2321b5a78feb98c90765316187c4ee8457a72667
ROHAN NIKAM <rohannikam@ROHANs-MacBook-Pro.local>
2020-04-01 12:12:21 +0530

    version.1.0

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, redirect, request
+from flask import Flask, jsonify, redirect, request, render_template
 from flask_cors import CORS
 import json
 import requests
@@ -6,16 +6,20 @@ from bs4 import BeautifulSoup
 url = "https://www.mohfw.gov.in/"
 
 app = Flask(__name__)
+app.secret_key = "Secret Key"
+
 CORS(app)  # for cross site request
 # app.config['JSON_SORT_KEYS'] = False
 
+l = []
+t = []
 
 @app.route('/')
 def index():
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
     myData = ""
-    for tr in soup.find(id="cases").find_all('tr'):
+    for tr in soup.find(id="state-data").find_all('tr'):
         myData += tr.get_text()
 
     myData = myData.split('\n')
@@ -24,7 +28,7 @@ def index():
     for i in range(len(myData)):
         if myData[i]:
             res.append(myData[i])
-
+    global final
     final = []
     total = []
     i = 5
@@ -39,19 +43,37 @@ def index():
 
     label = [
         "Name", "Confirmed", "Cured", "Death"]
-
+    global l
     l = []
     for i in final:
         d = dict(zip(label, i))
         l.append(d)
 
     tlabel = ["Total_Confirmed", "Total_Cured", "Total_Death"]
+
+    global t
     t = []
     for i in total:
         d = dict(zip(tlabel, i))
         t.append(d)
-    return jsonify({"state": l, "total": t})
+    
+    #return jsonify(myData)
+    #return jsonify({"state": l, "total": t})
+    return render_template('index.html', country=t, state=l)
 
+@app.route('/state', methods = ['GET', 'POST'])
+def state():
+    if request.method == "GET":
+        #name = request.args.get('n')
+        return jsonify(l)
+        
+
+
+
+@app.route('/country')
+def country():
+    return jsonify(t)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
+    
